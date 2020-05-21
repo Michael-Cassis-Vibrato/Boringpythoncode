e32430fbd742badf9e3490fa41994927e2bf348e
Michael Cassis <45756135+Michael-Cassis-Vibrato@users.noreply.github.com>
2020-05-21 11:56:17 +1000

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,20 @@
+
+
+print()
+print('Hello World')
+print('What is your name')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print()
+print('What is your age?')
+myAge = input()
+if myAge > str(int(30)):
+    print('Ewwwwww so old')
+else:
+    print('Wow so young!')
+print() 
+print('You will be ' + str(int(myAge) +1) + ' in a year.')
+
+
